155b02dc1d04a7db33d6b6ffbeb9a1cb581a938c
NeethuVinodan <48319144+NeethuVinodan@users.noreply.github.com>
2019-03-08 15:30:59 +0530

    This is the first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JosephusProblem
+{
+    public class Josephus
+    {
+        public static void Main()
+        {
+            JosephFunctions ob1 = new JosephFunctions();
+            Console.WriteLine("Enter the number of person in the game");
+            int n = Convert.ToInt32(Console.ReadLine());
+            // if number k which indicates that k-1 persons are skipped and kth person is killed in circle.
+            Console.WriteLine("Enter the number of persons to be skipped");
+            int k = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The person who survived is " + ob1.josephus(n, k));
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
---
+++ josephFunctions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JosephusProblem
+{
+    class JosephFunctions
+    {
+        public int josephus(int n, int k)
+        {
+            if (n == 1)
+                return 1;
+            else
+                 return (josephus(n - 1, k) + k - 1) % n + 1;
+        }
+    }
+}
+
